fdcd8004c19ea89756dbcb6c04982387b97a444e
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-04-08 21:41:02 +0300

    Fundamentals Module

---
+++ Fundamentals with Java/T15 - Bitwise Operations/src/TribitSwitch.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TribitSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = Long.parseLong(scanner.nextLine());
+        int p  = Integer.parseInt(scanner.nextLine());
+
+        long mask = (long) 7 << p;
+        long result = n ^ mask;
+
+        System.out.println(result);
+    }
+}
