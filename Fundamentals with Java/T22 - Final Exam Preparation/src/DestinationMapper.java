db477fb55347507e73d07b16020a1835c552f157
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-04-19 21:44:03 +0300

    Fundamentals Module

---
+++ Fundamentals with Java/T22 - Final Exam Preparation/src/DestinationMapper.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DestinationMapper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        Pattern pattern = Pattern.compile("([=/])(?<destination>[A-Z][A-Za-z]{2,})\\1");
+        Matcher matcher = pattern.matcher(input);
+        List <String> destinations = new ArrayList<>();
+        int travelPoints = 0;
+
+        boolean foundSomething = matcher.find();
+
+        while (foundSomething) {
+            String destination = matcher.group("destination");
+            destinations.add(destination);
+            travelPoints += destination.length();
+            foundSomething = matcher.find();
+        }
+        System.out.println("Destinations: " + String.join(", ", destinations));
+        System.out.println("Travel Points: " + travelPoints);
+    }
+}
