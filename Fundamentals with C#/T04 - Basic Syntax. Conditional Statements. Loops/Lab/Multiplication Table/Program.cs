1a09160e45883e389aca6ad9b074620da6b90058
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-05-03 18:35:03 +0300

    Fundamentals Module

---
+++ Fundamentals with C#/T04 - Basic Syntax. Conditional Statements. Loops/Lab/Multiplication Table/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Multiplication_Table;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{n} X {i} = {n*i}");
+        }
+    }
+}
---
+++ Fundamentals with C#/T04 - Basic Syntax. Conditional Statements. Loops/Lab/Student Information/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Information;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string studentName = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        double averageGrade = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:F2}");
+    }
+}
