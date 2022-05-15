9f13b6586fa2f97e8e13451779f5494c365af1f5
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-05-15 20:20:02 +0300

    C# Fundamentals / T06

---
+++ Fundamentals with C#/T06 - Data Types and Variables/exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CONSUMED_BY_WORKERS = 26;
+
+            int spices      = int.Parse(Console.ReadLine());
+            int days        = 0;
+            int totalSpices = 0;
+
+            while (spices >= 100)
+            {
+                totalSpices += spices - CONSUMED_BY_WORKERS;
+                spices -= 10;
+                days++;
+
+                if (spices < 100)
+                {
+                    totalSpices -= CONSUMED_BY_WORKERS;
+                }
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(totalSpices);
+        }
+    }
+}
---
+++ Fundamentals with C#/T06 - Data Types and Variables/lecture/09. Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCharacter  = char.Parse(Console.ReadLine());
+            char secondCharacter = char.Parse(Console.ReadLine());
+            char thirdCharacter  = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstCharacter}{secondCharacter}{thirdCharacter}");
+        }
+    }
+}
