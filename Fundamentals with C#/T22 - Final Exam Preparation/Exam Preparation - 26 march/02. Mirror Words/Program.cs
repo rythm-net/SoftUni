3a30a5be39a035cf424497e1aa43e88020a9d4ec
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-06-02 08:50:24 +0300

    C# Fundamentals / T22

---
+++ Fundamentals with C#/T22 - Final Exam Preparation/Exam Preparation - 26 march/02. Mirror Words/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Mirror_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary<string, string> mirrors = new Dictionary<string, string>();
+            // List<string> mirrors = new List<string>();
+
+            string text = Console.ReadLine();
+            string pattern = @"([@#])(?<word>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1";
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+            int countOfMirrorWords = 0;
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine($"No word pairs found!");
+            }
+            else
+            {
+                List<string> mirrorWords = new List<string>();
+                Console.WriteLine($"{matches.Count} word pairs found!");
+
+                foreach (Match match in matches)
+                {
+                    string firstWord = match.Groups["word"].Value;
+                    string secondWord = match.Groups["secondWord"].Value;
+                    string reversed = string.Join("", secondWord.Reverse());
+
+                    if (firstWord == reversed)
+                    {
+                        countOfMirrorWords++;
+                        mirrorWords.Add($"{firstWord} <=> {secondWord}");
+                    }
+                }
+                if (countOfMirrorWords == 0)
+                {
+                    Console.WriteLine($"No mirror words!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"The mirror words are:");
+                    Console.WriteLine(string.Join(", ", mirrorWords));
+                }
+            }
+            if (countOfMirrorWords == 0)
+            {
+                Console.WriteLine($"No mirror words!");
+            }
+        }
+    }
+}
