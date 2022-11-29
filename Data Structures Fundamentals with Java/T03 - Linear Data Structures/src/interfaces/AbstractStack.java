a0c5b57ef433614a3f52bf585395f9cf0b8ca9e8
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2022-11-29 22:08:16 +0200

    Java Data Structures Fundamentals / T03

---
+++ Data Structures Fundamentals with Java/T03 - Linear Data Structures/src/interfaces/AbstractStack.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public interface AbstractStack<E> extends Iterable<E> {
+
+    void push(E element);
+
+    E pop();
+    E peek();
+
+    int size();
+    boolean isEmpty();
+}
