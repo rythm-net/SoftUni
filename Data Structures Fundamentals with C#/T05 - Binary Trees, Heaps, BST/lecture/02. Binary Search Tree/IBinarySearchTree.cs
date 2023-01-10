71e4fedeaa698489c05933c0427a3ab0fda6b53b
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2023-01-10 17:14:52 +0200

    C# Data Structures Fundamentals / T05

---
+++ Data Structures Fundamentals with C#/T05 - Binary Trees, Heaps, BST/lecture/02. Binary Search Tree/IBinarySearchTree.cs
@@ -0,0 +1,12 @@
+﻿namespace _02._Binary_Search_Tree
+{
+    using System;
+
+    public interface IBinarySearchTree<T> where T : IComparable<T>
+    {
+        void Insert(T element);
+        bool Contains(T element);
+        void EachInOrder(Action<T> action);
+        IBinarySearchTree<T> Search(T element);
+    }
+}
---
+++ Data Structures Fundamentals with C#/T05 - Binary Trees, Heaps, BST/lecture/03. Max Heap/IAbstractHeap.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._Max_Heap
+{
+    using System;
+
+    public interface IAbstractHeap<T> 
+        where T : IComparable<T>
+    {
+        int Size { get; }
+
+        void Add(T element);
+
+        T Peek();
+
+        T ExtractMax();
+    }
+}
