9179ce5450c6f6f472d5b9532a5f0a316843f210
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2023-03-16 18:06:29 +0200

    C# Algorithms Fundamentals / T02

---
+++ Algorithms Fundamentals with C#/Т02 - Recursion and Backtracking/lecture/03. Generating 01 Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Generating_01_Vectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+            Gen01(arr, 0);
+        }
+
+        private static void Gen01(int[] arr, int idx)
+        {
+            if (idx >= arr.Length)
+            {
+                Console.WriteLine(String.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[idx] = i;
+                Gen01(arr, idx + 1);
+            }
+        }
+    }
+}
