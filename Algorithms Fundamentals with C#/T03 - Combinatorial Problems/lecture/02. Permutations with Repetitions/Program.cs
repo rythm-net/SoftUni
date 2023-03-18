086adf43390dcda5b6aa9d9babecf7097ba4946c
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2023-03-18 14:38:42 +0200

    C# Algorithms Fundamentals / T03

---
+++ Algorithms Fundamentals with C#/T03 - Combinatorial Problems/lecture/02. Permutations with Repetitions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Permutations_with_Repetition
+{
+    internal class Program
+    {
+        private static string[] elements;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(idx + 1);
+
+            var used = new HashSet<string> { elements[idx] };
+
+            for (int i = idx + 1; i < elements.Length; i++)
+            {
+                if (!used.Contains(elements[i]))
+                {
+                    Swap(idx, i);
+                    Permute(idx + 1);
+                    Swap(idx, i);
+
+                    used.Add(elements[i]);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
---
+++ Algorithms Fundamentals with C#/T03 - Combinatorial Problems/lecture/06. Combinations with Repetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Combinations_with_Repetition
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+            combinations = new string[k];
+
+            Combinations(0, 0);
+        }
+
+        private static void Combinations(int idx, int elementsStartIndex)
+        {
+            if (idx >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIndex; i < elements.Length; i++)
+            {
+                combinations[idx] = elements[i];
+                Combinations(idx + 1, i);
+            }
+        }
+    }
+}
