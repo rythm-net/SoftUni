288b0ac40efbe3eaa70049542ca88286812eaa5d
Alexander Karaneichev <alexander.karaneichev@gmail.com>
2023-03-20 22:01:27 +0200

    C# Algorithms Fundamentals / T04

---
+++ Algorithms Fundamentals with C#/T04 - Searching, Sorting and Greedy Algorithms/lecture/01. Binary Search/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, number));
+        }
+
+        private static int BinarySearch(int[] numbers, int number)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+
+                if (numbers[mid] == number)
+                {
+                    return mid;
+                }
+
+                if (number > numbers[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algorithms Fundamentals with C#/T04 - Searching, Sorting and Greedy Algorithms/lecture/05. Quicksort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Quicksort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            QuickSort(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void QuickSort(int[] numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var pivot = start;
+            var left = start + 1;
+            var right = end;
+
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot] &&
+                    numbers[right] < numbers[pivot])
+                {
+                    Swap(numbers, left, right);
+                }
+
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left += 1;
+                }
+
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right -= 1;
+                }
+            }
+            Swap(numbers, pivot, right);
+
+            QuickSort(numbers, start, right - 1);
+            QuickSort(numbers, right + 1, end);
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
